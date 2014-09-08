31184c0f6ebadffffd8006e962c866c833df6b98
sidahmedmehani <sid.mehani@gmail.com>
2014-09-08 12:58:32 +0100

    sample domain  création de la classe Personne

---
+++ sample-domain/src/main/java/gr/javapapo/domain/entities/Personne.java
@@ -0,0 +1,53 @@
+package gr.javapapo.domain.entities;
+
+/**
+ * Created by hasni on 07/09/14.
+ */
+public class Personne {
+
+
+    private String nom;
+    private String prenom;
+    private String dateNaissance;
+    private String adresse;
+
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prénom) {
+        this.prenom = prénom;
+    }
+
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+
+
+
+
+
+}
